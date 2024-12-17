b77c9b1df6985aba65fbb251020b8d2e2f69b369
Oli <88791447+oli-ctrl@users.noreply.github.com>
2024-12-17 16:45:52 +0000

    add font loading to spriteutils

--- src/SpriteUtils.cpp
+++ src/SpriteUtils.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 
 // returns a sprite with a texture based on a given path
-sf::Sprite SpriteUtils::getSpriteFromPath(std::string path, sf::Texture &texture)
+sf::Sprite
+SpriteUtils::getSpriteFromPath(std::string path, sf::Texture& texture)
 {
   sf::Sprite sprite;
   if (texture.loadFromFile(path))
@@ -10,12 +11,12 @@ sf::Sprite SpriteUtils::getSpriteFromPath(std::string path, sf::Texture &texture
     sprite.setTexture(texture);
     return sprite;
   }
-  std::cout << "Failed to load Texture from path " << path;
+  std::cout << "Failed to load Texture from path " << path << "\n";
   return sprite;
 }
 
-sf::Sprite
-SpriteUtils::getSpriteFromPath(std::string path, sf::Texture& texture, int scale)
+sf::Sprite SpriteUtils::getSpriteFromPath(
+  std::string path, sf::Texture& texture, float scale)
 {
   sf::Sprite sprite;
   if (texture.loadFromFile(path))
@@ -26,4 +27,40 @@ SpriteUtils::getSpriteFromPath(std::string path, sf::Texture& texture, int scale
   }
   std::cout << "Failed to load Texture from path " << path << "\n";
   return sprite;
+}
+
+sf::Font SpriteUtils::getFontFromPath(std::string path)
+{
+  sf::Font temp_font;
+  if (!temp_font.loadFromFile(path))
+  {
+    std::cout << "Failed to load Font from path " << path << "\n";
+  }
+  return temp_font;
+}
+
+sf::Text SpriteUtils::setupText(
+  sf::Font& font, std::string string, int character_size, sf::Color color,
+  int x_pos, int y_pos)
+{
+  sf::Text temp_text;
+  temp_text.setFont(font);
+  temp_text.setString(string);
+  temp_text.setCharacterSize(character_size);
+  temp_text.setColor(color);
+  temp_text.setPosition(x_pos, y_pos);
+
+  return temp_text;
+}
+
+sf::Text SpriteUtils::setupText(
+  sf::Font& font, std::string string, int character_size, sf::Color color)
+{
+  sf::Text temp_text;
+  temp_text.setFont(font);
+  temp_text.setString(string);
+  temp_text.setCharacterSize(character_size);
+  temp_text.setColor(color);
+
+  return temp_text;
 }
--- src/SpriteUtils.h
+++ src/SpriteUtils.h
@@ -6,9 +6,18 @@
 
 namespace SpriteUtils
 {
-  sf::Sprite getSpriteFromPath(std::string path, sf::Texture &texture);
+  sf::Sprite getSpriteFromPath(std::string path, sf::Texture& texture);
   sf::Sprite
-  getSpriteFromPath(std::string path, sf::Texture &texture, int scale);
+  getSpriteFromPath(std::string path, sf::Texture& texture, float scale);
+
+  sf::Font getFontFromPath(std::string path);
+
+  sf::Text setupText(
+    sf::Font& font, std::string string, int character_size, sf::Color color,
+    int x_pos, int y_pos);
+
+  sf::Text setupText(
+    sf::Font& font, std::string string, int character_size, sf::Color color);
 }
 
 #endif
